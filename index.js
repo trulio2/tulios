fe1ef4a321ac724e7e4669f441ad3b4bc1ee8260
Tulio <sgepkajtk93x29@gmail.com>
2022-02-17 07:05:31 -0300

    add react

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import fs from 'fs'
+import 'dotenv'
+
+const app = express()
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(bodyParser.text())
+
+app.use('/paper-kit-react', (request, response) => {
+  const url = `build/${request.url}`
+  response.sendFile(url, { root: '.' })
+})
+
+app.get('*', (request, response) => {
+  response.sendFile('build/index.html', { root: '.' })
+})
+
+app.listen(process.env.PORT || 8080)
