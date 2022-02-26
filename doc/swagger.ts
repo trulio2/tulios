a78a3758b73200b49015de226ea3e8ccb27e1a1c
Tulio <sgepkajtk93x29@gmail.com>
2022-02-26 03:01:28 -0300

    test

--- doc/swagger.ts
+++
@@ -1,7 +0,0 @@
-import { DocumentBuilder } from '@nestjs/swagger';
-
-export const config = new DocumentBuilder()
-  .setTitle('TulioS API')
-  .setDescription('Main API')
-  .setVersion('0.0.1')
-  .build();
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { NestFactory } from '@nestjs/core';
-import { SwaggerModule } from '@nestjs/swagger';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { config } from '../doc/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TulioS API')
+    .setDescription('Main API')
+    .setVersion('0.0.1')
+    .build();
 
   const documment = SwaggerModule.createDocument(app, config);
 
